64c02dfc42209a66daf8d84dcb4de8d536dac574
punitvanjani <punit.vanjani@gmail.com>
2016-04-04 09:56:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,4 +4,5 @@ print "Today's timestamp is:", datetime.today()
 print "line added later", datetime.today()
 print "Later in day", datetime.today()
 print "Later in day2", datetime.today()
+print "Later in day3", datetime.today()
 
