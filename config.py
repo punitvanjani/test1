fcf383c0eb982529b189488fa0c4a07b04b7e5fd
punitvanjani <punit.vanjani@gmail.com>
2016-09-24 23:00:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import redis
 #test the git sync
-# line added for testing the auto sync 24-Sep-2016 4th CHANGE
+# line added for testing the auto sync 24-Sep-2016 4th CHANGEwew
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
